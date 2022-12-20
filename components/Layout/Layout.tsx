33afd27007587e53a309cad91a1aae243d60723c
Anthony <anthonyco@hotmail.com>
2022-12-20 15:21:19 -0500

    pre-render error fix

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,19 +1,24 @@
 import Head from 'next/head'
 import React from 'react'
 import { useStateContext } from '../../context/ContextWrap'
+import { Categories } from '../../lib'
 import CartModal from '../CartModal'
 import Footer from '../Footer'
 import Navbar from '../Navbar/Navbar'
 
 interface Props {
     children?: React.ReactNode,
-    categories: any,
-    currentRoute? : string
+    categories?: Categories[],
+    currentRoute?: string
 }
 
 const Layout: React.FC<Props> = ({ categories, children, currentRoute }) => {
     const { showCart } = useStateContext();
-    
+
+    if (categories === undefined) {
+        return <>{children}</>
+    }
+
     return (
         <div className='Layout'>
             <Head>
